6ea57a632e11422c2db7614196945fd4f0734281
신혁수 <122273080+sins051301@users.noreply.github.com>
2024-03-11 23:14:30 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import MovingDot from './Updating_Object/Cursor';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MovingDot/>
   </React.StrictMode>
 );
 
